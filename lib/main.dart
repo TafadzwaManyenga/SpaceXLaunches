1fc0c338750459b644f7df0176196487263ade1a
Tafadzwa Manyenga <tmanyenga@y7mail.com>
2021-07-15 22:19:53 +1000

    Added another container for basic application information.

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SpaceX Launches',
-      theme: ThemeData.dark(
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text('SpaceX Launches'),
-        ),
-        body: Container(
-          color: Colors.black,
-          child: Text("Rocket Launches"),
-        ),
-      ),
-    );
+        title: 'SpaceX Launches',
+        theme: ThemeData.dark(),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('SpaceX Launches'),
+            ),
+            body: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(5.5),
+                  padding: EdgeInsets.all(10),
+                  child: Text("Rocket Launches"),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 4, color: Colors.blueAccent),
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.lightBlueAccent,
+                          offset: Offset(6.0, 6.0),
+                        )
+                      ]),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(5.5),
+                  padding: EdgeInsets.all(10),
+                  child: Text("About"),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 4, color: Colors.green),
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.lightGreenAccent,
+                          offset: Offset(6.0, 6.0),
+                        )
+                      ]),
+                )
+              ],
+            )));
   }
 }
